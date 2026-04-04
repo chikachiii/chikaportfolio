dcb8231513cd9107a8e3a2371ee019b9eaa0c129
chikachiii <email_anda_di_github@example.com>
2026-04-04 22:32:37 +0700

    Update profile description

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -18,7 +18,7 @@ class ProfileSeeder extends Seeder
             'birth_date' => '2008-11-21',
             'school' => 'SMAK Frateran',
             'class' => '12 E',
-            'description' => 'Seorang siswi SMAK Frateran kelas 12 E yang memiliki minat di Art & Creativity.',
+            'description' => 'Hi! I’m someone who loves expressing creativity in different forms.🎨 From art and fashion to photography and content creation, I enjoy expressing myself in fun and unique ways. Always exploring, always creating ✨',
             'profile_image' => null,
             'created_at' => now(),
             'updated_at' => now(),
